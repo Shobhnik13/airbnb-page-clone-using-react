e2b92ea42f7e55515f497acd65a31d783fe15c98
shobhnik13 <shobhnikw@gmail.com>
2022-12-10 14:47:42 +0530

    improved props

---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+export default[
+    {
+        id:1,
+        title:"Life lessons with Katie Zaferes",
+        price:135,
+        coverImg:"../images/card1.svg",
+        stats:{
+            rating:5.0,
+            reviewCount:6
+        },
+        location:"USA",
+        openSpots:28,
+    },
+    {
+        id:2,
+        title:"Learn Wedding photography",
+        price:325,
+        coverImg:"../images/card2.svg",
+        stats:{
+            rating:3.5,
+            reviewCount:8
+        },
+        location:"online",
+        openSpots:10,
+    },
+    {
+        id:3,
+        titlfe:"Group mountain biking",
+        price:120,
+        coverImg:"../images/card3.svg",
+        stats:{
+            reating:4.0,
+            reviewCount:12
+        },
+        location:"online",
+        openSpots:0,
+    }
+]
